70fcde17a1b6573a3a8b8af536bec6578de06801
gitMykola <primusbandatech@gmail.com>
2019-02-21 15:07:52 +0200

    BrowserAnimationsModule added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="row">
+      <div class="columns">
+        <button (click)="toggleMove()">Press me for animation</button>
+      </div>
+      <div class="columns">
+        <div id="content" [@focusPanel]='state' [@movePanel] = 'state'>Look at me animation</div>
+      </div>
+    </div>
+  `,
+  styles: [`
+    button { font-size: 1.8em; padding: 30px; border: 4px double black;}
+    #content { padding: 30px; background: #eeeeee; width: 200px; margin: 10px; border: 4px dashed green; }
+  `],
+  animations: [
+    trigger('focusPanel', [
+      state('inactive', style ({
+        transform: 'scale(1)',
+        backgroundColor: '#eee'
+      })),
+      state('active', style ({
+        transform: 'scale(1.1)',
+        backgroundColor: '#cfd8dc'
+      })),
+      transition('inactive => active', animate('100ms this.ease-in ')),
+      transition('active => inactive', animate('100ms this.ease-out '))
+    ]),
+    trigger('movePanel', [
+      transition('void => *' , [
+        animate (600, keyframes([
+          style({opacity: 0, transform: 'translateY(-200px)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(25px)', offset: .75}),
+          style({opacity: 1, transform: 'translateY(0)', offset: 1})
+        ]))
+      ])
+    ])
+  ],
 })
 export class AppComponent {
-  title = 'anim';
+  state = 'inactive';
+  toggleMove() {
+    this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+    console.log('Clicked');
+  }
 }
+
